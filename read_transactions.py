ef239b390e1a928cd5f93b7d03b40f1acc1c78da
sayefkhacodes <sayefkha20012018@gmail.com>
2026-06-21 21:17:13 +0100

    Calculate total from transactions

--- read_transactions.py
+++ read_transactions.py
@@ -1,5 +1,10 @@
 import csv
 with open("transactions.csv", "r") as file:
     reader = csv.reader(file)
+    next(reader)
+    total = 0
     for row in reader:
-        print(row)
+        amount = float(row[2])
+        total += amount
+        print(row[1], amount)
+    print("Total: £", total)
