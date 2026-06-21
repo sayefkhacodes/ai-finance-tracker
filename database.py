35d1607cd1abf19d60d3a136248f9fb7cffb9406
sayefkhacodes <sayefkha20012018@gmail.com>
2026-06-21 23:06:16 +0100

    Insert CSV transactions into SQLite database

--- database.py
+++ database.py
@@ -1,4 +1,6 @@
+import csv
 import sqlite3
+
 
 def create_database():
     conn = sqlite3.connect("finance.db")
@@ -18,4 +20,44 @@ def create_database():
     conn.close()
 
 
-create_database()
+def insert_transaction(date, description, amount, category=""):
+    conn = sqlite3.connect("finance.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT INTO transactions (date, description, amount, category)
+        VALUES (?, ?, ?, ?)
+    """, (date, description, amount, category))
+
+    conn.commit()
+    conn.close()
+
+
+def insert_from_csv(filename):
+    conn = sqlite3.connect("finance.db")
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM transactions")
+
+    with open(filename, "r") as file:
+        reader = csv.reader(file)
+        next(reader)
+
+        for row in reader:
+            date = row[0]
+            description = row[1]
+            amount = float(row[2])
+
+            cursor.execute("""
+                INSERT INTO transactions (date, description, amount, category)
+                VALUES (?, ?, ?, ?)
+            """, (date, description, amount, ""))
+
+    conn.commit()
+    conn.close()
+    print("CSV data inserted into database!")
+
+
+if __name__ == "__main__":
+    create_database()
+    insert_from_csv("transactions.csv")
